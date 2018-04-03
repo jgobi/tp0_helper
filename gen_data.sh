46dd47639ff0f8427d2b4104010c0596979a88b9
Rafael Castro <rafael.castro@dcc.ufmg.br>
2018-04-03 15:14:45 -0300

    data saver "functional"

--- gen_data.sh
+++ gen_data.sh
@@ -1,7 +1,11 @@
-echo "" > data.txt
-for i in "seq $1 $3 $2";
+echo $1 $3 $2 > timedata.txt
+for i in $(seq $1 $3 $2);
 do
     python gen_data.py --output graph_data;
-    (time ($4 graph_data.in > graph_data.out)) >> data.txt
+    time_old=$(date +%s%N)
+    $4 graph_data.in > graph_data.out
+    time_new=$(date +%s%N)
+    let timediff=time_new-time_old
+    echo $i $timediff
+    (echo $i $timediff >> timedata.txt)
 done
-rm graph_data.in graph_data.out
